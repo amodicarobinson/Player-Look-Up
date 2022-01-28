915ca1363d46637df7c2e4e0ac827a559cfb6fc1
Your Name <modicarobinsona@gmail.com>
2022-01-28 16:43:57 -0700

    Created an .exe to run program

---
+++ #Open google in python - Windows.py
@@ -0,0 +1,6 @@
+#Open google in python - Windows
+import webbrowser    
+urL='https://www.google.com'
+chrome_path="C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+webbrowser.register('chrome', None,webbrowser.BackgroundBrowser(chrome_path))
+webbrowser.get('chrome').open_new_tab(urL)
--- Player Lookup App.py
+++ Player Lookup App.py
@@ -1,4 +1,6 @@
 from http.client import CONTINUE
+import sys
+import webbrowser
 
 
 while True:
@@ -13,14 +15,16 @@ while True:
 
 	for j in search(query, tld="co.in", num=10, stop=10, pause=2):
 		print(j)
+	
+	
 
-while True:
-        answer = str(input('Run again? (y/n): '))
-        if answer in ('y', 'n'):
-            break
-        print("invalid input.")
-if answer == 'y':
-	CONTINUE
-else:
-    print("Goodbye")
-    sys.exit()
+	while True:
+		answer = str(input("Run again? (y/n): "))
+		if answer in ('y', 'n'):
+			break
+		print("invalid input.")
+	if answer == 'y':
+		CONTINUE
+	else:
+		print("Goodbye")
+		sys.exit()
